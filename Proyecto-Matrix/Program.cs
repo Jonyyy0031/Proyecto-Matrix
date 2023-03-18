ecea9e55fe4d91b5b04787fc61b6925976f5b2c6
Jonyyy0031 <jaxelhm16@gmail.com>
2023-03-18 13:03:29 -0500

    Test SC

--- Proyecto-Matrix/Program.cs
+++ Proyecto-Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Spectre.Console;
 
 namespace Proyecto_Matrix
 {
@@ -6,9 +7,37 @@ namespace Proyecto_Matrix
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            asdsada
-                asdasdasda
+            AnsiConsole.Write(
+                new FigletText("MATRIX")
+                    .LeftJustified()
+                    .Color(Color.Red));
+            Console.WriteLine("");
+            var Funcion = AnsiConsole.Prompt(
+         new SelectionPrompt<string>()
+        .Title("¿Que desea Realizar?")
+        .PageSize(10)
+        .MoreChoicesText("Mueve con las flechas de arriba y abajo para seleccionar")
+        .AddChoices(new[] {
+            "Registro de productos", "Visualizacion de inventario", "Actualizacion de inventario",
+            "Checkeo de producto"
+        }));
+            if (Funcion == "Registro de productos")
+            {
+                Console.WriteLine(Funcion);
+            }
+            if (Funcion == "Visualizacion de Inventario")
+            { 
+                Console.WriteLine(Funcion); 
+            }
+            if (Funcion == "Actualizacion de Inventario")
+            {
+                Console.WriteLine(Funcion);
+            }
+            if (Funcion =="Checkeo de producto")
+            {
+                Console.WriteLine(Funcion);
+            }
+            
         }
     }
 }
