b7b9bf0137ce07ca3b56eaf3c05d00f407a0f05e
Jonyyy0031 <jaxelhm16@gmail.com>
2023-03-18 21:11:42 -0500

    New Carpets,Class DbContext

---
+++ Proyecto-Matrix/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Spectre.Console;
+
+namespace Proyecto_Matrix.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=JONY;Database=ProyectoDB-Matrix;Trusted_Connection=True;");
+        }
+    }
+}
