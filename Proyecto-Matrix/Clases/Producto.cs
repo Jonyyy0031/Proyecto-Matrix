b2d6a0c74cc4a9f4b8ffbd6d77b84903c4b35dbd
Edifurro <128158575+Edifurro@users.noreply.github.com>
2023-03-20 18:03:20 -0500

    actualizacion de clases
    actualizacion de funciones y clases

--- Proyecto-Matrix/Clases/Producto.cs
+++ Proyecto-Matrix/Clases/Producto.cs
@@ -12,7 +12,8 @@ namespace Proyecto_Matrix.Clases
         public string descripcion { get; set; }
         public int cantidad_inventario { get; set; }
         public string precio { get; set; }
-        public string marca { get; set; }
+        public string IDproducto { get; set; }//como el codigo de barras w un id de 4 digitos alv
+        
 
 
 
