90ace510ac047682a5bf8ade5f2867ef3580a7d8
Edifurro <128158575+Edifurro@users.noreply.github.com>
2023-03-20 17:55:49 -0500

    actualizacion de funciones
    te adjunto la actualizacion de las funciones y las clases, perdon no lo volvere a hacer

--- Proyecto-Matrix/Funciones/inventario.cs
+++ Proyecto-Matrix/Funciones/inventario.cs
@@ -4,28 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Proyecto_Matrix.Clases
+namespace Proyecto_Matrix.Funciones
 {
-    internal class Inventario
+    internal class inventario
     {
         //te adjunto unos voids q servirian gay
-       //aqui seria con lo que el poderoso estructura de datos no ha enseñado de ordenamientos
+        //aqui seria con lo que el poderoso estructura de datos no ha enseñado de ordenamientos
 
 
 
         public void agregarbotepintura()
         {
-         
+
         }
 
-        public void  quitarbotepintura()
+        public void quitarbotepintura()
         {
-           
+
         }
 
         public void actualizarinventario()
         {
-            
+
         }
     }
 }
