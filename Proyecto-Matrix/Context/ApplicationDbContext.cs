c06abe91304a9d78a1aa4350ced52c92290db8a1
Edifurro <128158575+Edifurro@users.noreply.github.com>
2023-03-21 13:18:17 -0500

    table

--- Proyecto-Matrix/Context/ApplicationDbContext.cs
+++ Proyecto-Matrix/Context/ApplicationDbContext.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Proyecto_Matrix.Clases;
 using Spectre.Console;
 
 namespace Proyecto_Matrix.Context
@@ -13,7 +14,9 @@ namespace Proyecto_Matrix.Context
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=JONY;Database=ProyectoDB-Matrix;Trusted_Connection=True;");
-            
+
         }
+       public DbSet<Producto> productos { get; set; }
+        public DbSet<Persona> personas { get; set; }
     }
 }
